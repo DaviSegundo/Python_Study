2f6e695a13648f862be9a138621a641b95d7e0ac
Davi S.P <davisp2009@hotmail.com>
2022-07-15 01:41:02 -0300

    SOLID Principles V1

---
+++ code_organization/solid_principles/4_interface_segregation_before.py
@@ -0,0 +1,87 @@
+from abc import ABC, abstractmethod
+
+class Order:
+    items = []
+    quantities = []
+    prices = []
+    status = "open"
+
+    def add_item(self, name, quantity, price):
+        self.items.append(name)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+
+    def total_price(self):
+        total = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i] * self.prices[i]
+        return total
+
+
+class PaymentProcessor(ABC):
+    
+    @abstractmethod
+    def auth_sms(self, code):
+        pass
+
+    @abstractmethod
+    def pay(self, order):
+        pass
+
+class DebitPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, security_code) -> None:
+        self.security_code = security_code
+        super().__init__()
+
+    def auth_sms(self, code):
+        print(f'Verifying SMS code: {code}')
+        self.verified = True
+
+    def pay(self, order):
+        if not self.verified:
+            raise Exception('Not authorized')
+        print('Processing debit paymente type')
+        print(f'Verifying security code: {self.security_code}')
+        order.status = 'paid'
+
+class CreditPaymentProcessor(PaymentProcessor):
+    def __init__(self, security_code) -> None:
+        self.security_code = security_code
+        super().__init__()
+
+    def auth_sms(self, code):
+        raise Exception('Credit card payments dont support SMS code authorization')
+
+    def pay(self, order):
+        print('Processing credit paymente type')
+        print(f'Verifying security code: {self.security_code}')
+        order.status = 'paid'
+
+class PayPalPaymentProcessor(PaymentProcessor):
+
+    def __init__(self, email_address) -> None:
+        self.email_address = email_address
+        super().__init__()
+
+    def auth_sms(self, code):
+        print(f'Verifying SMS code: {code}')
+        self.verified = True
+
+    def pay(self, order):
+        print('Processing paypal paymente type')
+        print(f'Verifying security code: {self.email_address}')
+        order.status = 'paid'
+
+
+if __name__ == '__main__':
+    order = Order()
+
+    order.add_item("Keyboard", 1, 50)
+    order.add_item("SSD", 1, 150)
+    order.add_item("USB Cable", 2, 5)
+
+    print(order.total_price())
+
+    processor = PayPalPaymentProcessor("davisp@hotmail.com")
+    processor.pay(order)

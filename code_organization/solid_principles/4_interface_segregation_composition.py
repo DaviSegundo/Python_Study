fe6251e45609ccbb8fdbf8cfac2d84cf56a460eb
Davi S.P <davisp2009@hotmail.com>
2022-07-19 15:12:15 -0300

    SOLID Principles V2 - Final

--- code_organization/solid_principles/4_interface_segregation_composition.py
+++ code_organization/solid_principles/4_interface_segregation_composition.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Union
 
 class Order:
     items = []
@@ -7,16 +6,44 @@ class Order:
     prices = []
     status = "open"
 
-    def add_item(self, name: str, quantity: int, price: float) -> None:
+    def add_item(self, name, quantity, price):
         self.items.append(name)
         self.quantities.append(quantity)
         self.prices.append(price)
 
-    def total_price(self) -> float:
-        total: float = 0
+    def total_price(self):
+        total = 0
         for i in range(len(self.prices)):
             total += self.quantities[i] * self.prices[i]
         return total
+
+
+class Authorizer(ABC):
+
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+
+class SMSAuth(Authorizer):
+    authorized = False
+
+    def verify_code(self, code):
+        print(f"Verifying code: {code}")
+        self.authorized = True
+
+    def is_authorized(self):
+        return self.authorized
+
+class NotARobot(Authorizer):
+    authorized = False
+
+    def not_a_robot(self):
+        print("Are you a robot? Naaaa...")
+        self.authorized = True
+
+    def is_authorized(self):
+        return self.authorized
 
 
 class PaymentProcessor(ABC):
@@ -25,24 +52,16 @@ class PaymentProcessor(ABC):
     def pay(self, order):
         pass
 
-class PaymentProcessor_SMS(PaymentProcessor):
-    
-    @abstractmethod
-    def auth_sms(self, code: Union[str, int]):
-        pass
 
-class DebitPaymentProcessor(PaymentProcessor_SMS):
+class DebitPaymentProcessor(PaymentProcessor):
 
-    def __init__(self, security_code) -> None:
+    def __init__(self, security_code, authorizer: Authorizer) -> None:
+        self.authorizer = authorizer
         self.security_code = security_code
         super().__init__()
 
-    def auth_sms(self, code):
-        print(f'Verifying SMS code: {code}')
-        self.verified = True
-
     def pay(self, order):
-        if not self.verified:
+        if not self.authorizer.is_authorized():
             raise Exception('Not authorized')
         print('Processing debit paymente type')
         print(f'Verifying security code: {self.security_code}')
@@ -59,19 +78,16 @@ class CreditPaymentProcessor(PaymentProcessor):
         print(f'Verifying security code: {self.security_code}')
         order.status = 'paid'
 
-class PayPalPaymentProcessor(PaymentProcessor_SMS):
+class PayPalPaymentProcessor(PaymentProcessor):
 
-    def __init__(self, email_address) -> None:
+    def __init__(self, email_address, authorizer: Authorizer) -> None:
+        self.authorizer = authorizer
         self.email_address = email_address
         super().__init__()
 
-    def auth_sms(self, code):
-        print(f'Verifying SMS code: {code}')
-        self.verified = True
-
     def pay(self, order):
-        if not self.verified:
-            raise Exception('Not authorized')
+        if not self.authorizer.is_authorized():
+            raise Exception("Not authorized")
         print('Processing paypal paymente type')
         print(f'Verifying security code: {self.email_address}')
         order.status = 'paid'
@@ -86,6 +102,10 @@ if __name__ == '__main__':
 
     print(order.total_price())
 
-    processor = PayPalPaymentProcessor("davisp@hotmail.com")
-    processor.auth_sms(882697)
+    authorizer = SMSAuth()
+    # robot = NotARobot()
+
+    processor = PayPalPaymentProcessor("davisp@hotmail.com", authorizer)
+    authorizer.verify_code(882697)
+    # robot.not_a_robot()
     processor.pay(order)
